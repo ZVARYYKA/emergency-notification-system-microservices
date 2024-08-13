f6370e06b2cd238e0779906627710b2950021a43
ZVARYYKA <nzvarykin@mail.ru>
2024-08-13 15:32:50 +0300

    Create new microservice - receipts service for work with receipts of users, devices etc. Also set up security.

---
+++ recipient-service/src/test/java/dev/zvaryyka/recipientservice/RecipientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zvaryyka.recipientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
